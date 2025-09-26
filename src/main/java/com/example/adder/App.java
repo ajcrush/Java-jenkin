a215bb6e7db63eb15eeefe342255b329191f6d5c
Mohit sharma <mohitanand8987@gmail.com>
2025-09-26 10:19:47 +0530

    Initial Maven Project

---
+++ src/main/java/com/example/adder/App.java
@@ -0,0 +1,11 @@
+package com.example.adder;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+       int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        System.out.println("Sum is: " + (a + b));
+    }
+}
